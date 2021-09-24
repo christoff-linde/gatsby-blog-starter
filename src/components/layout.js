f09e9ad4cf3b5764c1ddb4a5cea0c3ea40d65bac
Christoff <christoff.linde@gmail.com>
2021-09-24 16:51:05 +0200

    Add global footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,80 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        <div className="global-footer">
+          <div>
+            <h4>My Animals &trade;</h4>
+            <p>&copy; TeamGeek</p>
+          </div>
+          <div>
+            <h5>About Us</h5>
+            <ul>
+              <li>
+                <Link to="">
+                  Locations
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  What we do
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  How we do it
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  Who we are
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5>Product</h5>
+            <ul>
+              <li>
+                <Link to="">
+                  Prints
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  Lorem ipsum
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  This is dummy text
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  Hello
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5>Contact Us</h5>
+            <ul>
+              <li>
+                <Link to="">
+                  work@teamgeek.io
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  +27 891 2008
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* © {new Date().getFullYear()}, Built with
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <a href="https://www.gatsbyjs.com">Gatsby</a> */}
       </footer>
     </div>
   )
