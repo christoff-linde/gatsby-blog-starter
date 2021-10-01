b434e7c69b9dd39e1ee23ec278dbcbd875b35993
Christoff <christoff.linde@gmail.com>
2021-10-01 12:52:38 +0200

    Add offline support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,3 +8,13 @@ import "./src/style.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
+
+export const registerServiceWorker = () => true
+
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(`This application has been updated. ` + `Reload to display the latest version?`);
+
+    if (answer === true) {
+        window.location.reload();
+    }
+}
